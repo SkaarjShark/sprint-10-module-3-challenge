0c95c9ee7c1fbaf8f30e54a177587c4e0d908bf5
Jacob Handley <skaarjshark@outlook.com>
2024-04-05 18:00:30 -0500

    sprint-10-module-3-challenge

---
+++ frontend/state/quotesSlice.js
@@ -0,0 +1,70 @@
+// ✨ create your `quotesSlice` in this module
+import { createSlice } from '@reduxjs/toolkit'
+
+let id = 1
+const getNextId = () => id++
+
+  const initialState = {
+    displayAllQuotes: true,
+    highlightedQuote: null,
+    quotes: [
+      {
+        id: getNextId(),
+        quoteText: "Don't cry because it's over, smile because it happened.",
+        authorName: "Dr. Seuss",
+        apocryphal: false,
+      },
+      {
+        id: getNextId(),
+        quoteText: "So many books, so little time.",
+        authorName: "Frank Zappa",
+        apocryphal: false,
+      },
+      {
+        id: getNextId(),
+        quoteText: "Be yourself; everyone else is already taken.",
+        authorName: "Oscar Wilde",
+        apocryphal: false,
+      },
+    ],
+  }
+
+export const quotesSlice = createSlice({
+  name: 'quotes',
+  initialState,
+  reducers: {
+    toggleVisibility(state) {
+      state.displayAllQuotes = !state.displayAllQuotes
+    },
+    deleteQuote(state, action) {
+      state.quotes = state.quotes.filter(qt => qt.id !== action.payload)
+    },
+    editQuoteAuthenticity(state, action) {
+      const chosenQuote = state.quotes.find(qt => qt.id === action.payload)
+      chosenQuote.apocryphal = !chosenQuote.apocryphal
+    },
+    setHighlightedQuote(state, action) {
+      if (state.highlightedQuote === action.payload) state.highlightedQuote = null
+      else state.highlightedQuote = action.payload
+    },
+    createQuote(state, action) {
+      const newQuote = {
+        id: getNextId(),
+        quoteText: action.payload.quoteText,
+        authorName: action.payload.authorName,
+        apocryphal: false
+      }
+      state.quotes.push(newQuote)
+    }
+  }
+})
+
+export const {
+  createQuote,
+  deleteQuote,
+  editQuoteAuthenticity,
+  setHighlightedQuote,
+  toggleVisibility,
+} = quotesSlice.actions
+
+export default quotesSlice.reducer
